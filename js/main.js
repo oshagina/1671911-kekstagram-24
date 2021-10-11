3ddb42d58f0fe871cf2aeca27132cd80e039f0b3
oshagina6 <shaginao04@gmail.com>
2021-10-11 21:49:44 +0200

    исправлены ошибки указанные eslint

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-function getSum(a,b){
-const description='Сумма двух чисел равна';
-const sum=a+b;
-return description+' '+sum;
-};
+function getSum(aaa,bbb){
+  const description='Сумма двух чисел равна';
+  const sum=aaa+bbb;
+  return `${description  } ${ sum}`;
+}
 getSum(1,2);
 
 const randomInteger = (min, max) => {
-    if(min>=0&&max>0&&max>min){
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-    alert('error!!!');
-    };
-  
+  if(min>=0&&max>0&&max>min){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+};
+randomInteger(5,10);
+
 randomInteger(10,20);
 
 //Функция для проверки максимальной длины строки
 function checkWordLength(word, maxLength){
-    if (word.length>maxLength){
-        return false;
-    } 
-        return true;
-};
+  if (word.length>maxLength){
+    return false;
+  }
+  return true;
+}
 checkWordLength('Olga',3);
