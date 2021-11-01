f01f901b81a68fd91a900448b05eb658f8dc17a8
oshagina6 <shaginao04@gmail.com>
2021-11-01 15:11:27 +0100

    Отрисуй меня полностью (часть 1)

--- js/utils/miniatures.js
+++ js/utils/miniatures.js
@@ -7,14 +7,16 @@ const pictures=getArrayDescriptionsAllFotos();
 const pictureTemplateContent=document.querySelector('#picture').content;
 //В фрагменте находим  элемент а
 const pictureTemplateElement=pictureTemplateContent.querySelector('a');
+
 //создаём "коробочку"
 const pictureListFragment=document.createDocumentFragment();
 
 pictures.forEach((url, comments,likes) => {
   const fotoElement=pictureTemplateElement.coneNode(true);// Клонируем элемент со всеми "внутренностями"
+  const pictureInfoElement=fotoElement.querySelector('.picture__info');
   fotoElement.querySelector('.picture__img').src=url;
-  fotoElement.querySelector('.picture__info').content.querySelector('.picture__comments').textContent=comments;
-  fotoElement.querySelector('.picture__info').content.querySelector('.picture__likes').textContent=likes;
+  pictureInfoElement.content.querySelector('.picture__comments').textContent=comments;
+  pictureInfoElement.content.querySelector('.picture__likes').textContent=likes;
   pictureListFragment.appendChild(fotoElement);// Складываем созданные элементы в "коробочку"
 });
 pictures.appendChild(pictureListFragment); // отрисовываем всё из "коробочки"
